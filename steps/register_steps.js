adef2819df133bb299f18414b23b48f778f9bcb9
giangpk33@gmail.com <giangpk33@gmail.com>
2022-09-08 18:23:34 +0700

    Update case3

--- steps/register_steps.js
+++ steps/register_steps.js
@@ -46,32 +46,12 @@ module.exports = {
     commonSteps.seeElement(registerPage.register.userNameTextBox);
 
     // //Fill the form
-    // commonSteps.fillFieldInput(registerPage.register.userNameTextBox, randomUserName);
-    // commonSteps.fillFieldInput(registerPage.register.emailTextBox, randomEmail);
-    // commonSteps.fillFieldInput(registerPage.register.passwordTextBox, randomPassword);
-    // commonSteps.fillFieldInput(registerPage.register.nameTextBox, randomName);
-
-    // this.selectProgrammingLanguage(randomPL);
-    
-    // commonSteps.tapToButton(registerPage.register.acceptAddsCheckBox);
     this.fillRegisterForm(randomName, randomUserName, randomPassword, randomEmail, randomPL, randomCheckAddson);
 
     //Click on Register btn
     commonSteps.tapToButton(registerPage.register.registerUserButton);
 
     //Verify information
-    // commonSteps.seeElement(registerPage.registerSuccess.nameTextView);
-    // commonSteps.seeText(randomName);
-    // commonSteps.seeElement(registerPage.registerSuccess.userNameTextView);
-    // commonSteps.seeText(randomUserName);
-    // commonSteps.seeElement(registerPage.registerSuccess.passwordTextView);
-    // commonSteps.seeText(randomPassword);
-    // commonSteps.seeElement(registerPage.registerSuccess.emailTextView);
-    // commonSteps.seeText(randomEmail);
-    // commonSteps.seeElement(registerPage.registerSuccess.programmingLanguageTextView);
-    // commonSteps.seeText(randomPL);
-    // commonSteps.seeElement(registerPage.registerSuccess.acceptAddsTextView);
-    // commonSteps.seeText(randomCheckAddson);
     this.verifyInformation(randomName, randomUserName, randomPassword, randomEmail, randomPL, randomCheckAddson);
 
     //Click on Register button
